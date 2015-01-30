b4ce25d6fddb5629222eeae98352ce6a833269de
Ben Kent <kentb@wit.edu>
2015-01-30 18:52:44 -0500

    Changes to BuildState + BattleState created

--- core/src/com/totemdefender/states/BattleState.java
+++ core/src/com/totemdefender/states/BattleState.java
@@ -1,47 +1,37 @@
 package com.totemdefender.states;
 
-import com.badlogic.gdx.utils.Timer;
-import com.badlogic.gdx.utils.Timer.Task;
 import com.totemdefender.TotemDefender;
 
+public class BattleState implements State {
 
-public class BuildState implements State {
-	
-	
-	StateManager statemanager;
-	Timer timer;
-	Task task;
-	
 	@Override
 	public boolean canEnter(TotemDefender game) {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
 	@Override
-	public void onEnter(final TotemDefender game) {
-		
-		timer.scheduleTask(new Task(){
-			
-			public void run(){
-				onExit(game);
-			}
-			
-		}, 4000*60);
+	public void onEnter(TotemDefender game) {
+		// TODO Auto-generated method stub
 		
 	}
+
 	@Override
 	public void onExit(TotemDefender game) {
-		statemanager.attachState(new BattleState());
+		// TODO Auto-generated method stub
 		
 	}
+
 	@Override
 	public boolean canExit(TotemDefender game) {
 		// TODO Auto-generated method stub
 		return false;
 	}
+
 	@Override
 	public void update(TotemDefender game) {
 		// TODO Auto-generated method stub
 		
 	}
+
 }
