47b279e9b906ae1c1104d5bb9a8b28f926ae821e
Viter Nguyen <retiv42@gmail.com>
2015-01-31 13:56:45 -0500

    add in new Menu class

--- core/src/com/totemdefender/menu/Menu.java
+++ core/src/com/totemdefender/menu/Menu.java
@@ -1,42 +1,96 @@
 package com.totemdefender.menu;
-import java.awt.Component;
-import com.badlogic.gdx.InputMultiplexer;
+import java.util.ArrayList;
+
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.InputProcessor;
 
 //Base menu from which all other menus derive. Should provide the ability
 //to add Buttons and Text. Should have its own input processor which its component buttons will attach to. 
 
-public abstract class Menu implements InputProcessor{
+public class Menu implements InputProcessor{
 	
-	public abstract void render();
+	private ArrayList<Component> cmpList = new ArrayList<Component>();
+	private boolean shouldRender;
+
+	void render(){
+		
+	}
 	
 	//Allows menu to be rendered, attaches inputprocessor to main multiplexer
-	public void show(InputMultiplexer multiplexer) {
-		multiplexer.addProcessor(this);
-	}
+	public void show()
+	{ setShouldRender(true); }
 	
 	//Stops menu rendering, detaches inputprocessor from multiplexer.
-	public void hide(InputMultiplexer multiplexer) {
-		multiplexer.removeProcessor(this);
-	}
+	public void hide()
+	{ setShouldRender(false); }
 	
 	//Adds a component (Should attach to inputprocessor if menu is visible)
-	public void addComponent(Component cmp){
-		//don't know how to add a component to inputprocessor try override
-	}
+	public void addComponent(Component cmp)
+	{ cmpList.add(cmp); }
 	
 	//Removes a component
-	public void removeComponent(Component cmp){
-		//don't know how to remove a component to inputprocessor try override
-	} 
+	public void removeComponent(Component cmp)
+	{ cmpList.remove(cmp); } 
 	
 	//Adds a listener to the listener list
-	public void addListener(InputProcessor listener, InputMultiplexer multiplexer){
-		multiplexer.addProcessor(listener);
-	}
+	public void addListener(InputProcessor listener, ArrayList<InputProcessor> list)
+	{ list.add(listener); }
 
 	//Removes a listener
-	public void removeListener(InputProcessor listener, InputMultiplexer multiplexer){
-		multiplexer.removeProcessor(listener);
+	public void removeListener(InputProcessor listener, ArrayList<InputProcessor> list)
+	{ list.remove(listener); }
+
+	@Override
+	public boolean keyDown(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
 	}
+
+	@Override
+	public boolean keyUp(int keycode) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean keyTyped(char character) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean touchDragged(int screenX, int screenY, int pointer) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean mouseMoved(int screenX, int screenY) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean scrolled(int amount) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean isShouldRender() 
+	{ return shouldRender; }
+
+	public void setShouldRender(boolean shouldRender) 
+	{ this.shouldRender = shouldRender; }
 }
