6b3d81ff1366c8f63d9fa955b753b43b7c88610e
Viter Nguyen <retiv42@gmail.com>
2015-02-01 17:07:25 -0500

    clean up button class

--- core/src/com/totemdefender/menu/Button.java
+++ core/src/com/totemdefender/menu/Button.java
@@ -10,19 +10,15 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Vector2;
 
 public class Button extends Component {
-<<<<<<< HEAD
 	private FreeTypeFontGenerator generator;
 	private FreeTypeFontParameter parameter;
 	private BitmapFont bitMapFont = generator.generateFont(parameter); // font size 12 pixels
 	private Vector2 textPosition;
 	
 	ShapeRenderer shapeMaker;
-=======
->>>>>>> 655f50c78fd46ed875747aa7b422115755907437
 	private String label;
 	private Color color;
 	
-	//gdx.files.internal("C/Windows/Fonts/Consolas")
 	public Button(String newLabel, Vector2 newSize, Vector2 newPosition, Color newColor) {
 		generator = new FreeTypeFontGenerator(Gdx.files.internal("assets/consola.ttf"));
 		FreeTypeFontParameter parameter = new FreeTypeFontParameter();
@@ -33,7 +29,6 @@ public class Button extends Component {
 		setSize(newSize); 
 		setPosition(newPosition);
 		setColor(newColor);
-<<<<<<< HEAD
 		
 		textPosition = new Vector2(this.getPosition().x + (this.getSize().x/2), this.getPosition().y + (this.getSize()).y);
 		shapeMaker = new ShapeRenderer();
@@ -50,7 +45,6 @@ public class Button extends Component {
 		shapeMaker.setColor(this.getColor());
 		shapeMaker.rect(this.getPosition().x, this.getPosition().y, this.getSize().x, this.getSize().y);
 		shapeMaker.end();
-=======
 	}
 	
 	public void render(SpriteBatch batch, ShapeRenderer shapeRenderer) {
@@ -58,7 +52,6 @@ public class Button extends Component {
 		shapeRenderer.setColor(this.getColor());
 		shapeRenderer.rect(this.getPosition().x, this.getPosition().y, this.getSize().x, this.getSize().y);
 		shapeRenderer.end();
->>>>>>> 655f50c78fd46ed875747aa7b422115755907437
 	}
 	
 	public boolean onButtonArea(int x, int y) {
