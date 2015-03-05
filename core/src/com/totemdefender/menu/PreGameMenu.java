e138152694eec77777612a340aa70f93696be768
Viter Nguyen <retiv42@gmail.com>
2015-03-05 13:34:33 -0500

    more changes with pre menu, still need instruct...

--- core/src/com/totemdefender/menu/PreGameMenu.java
+++ core/src/com/totemdefender/menu/PreGameMenu.java
@@ -7,107 +7,100 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.totemdefender.Level;
+import com.totemdefender.Player;
 import com.totemdefender.TotemDefender;
 import com.totemdefender.input.KeyboardEvent;
 import com.totemdefender.states.MainMenuState;
 import com.totemdefender.states.PresGameState;
 
 public class PreGameMenu extends NavigableContainer {
-	TextEntry player1, player2;
-	Button ready;
-	Button p1_weapon1, p1_weapon2, p1_weapon3;
-	Button p2_weapon1, p2_weapon2, p2_weapon3;
+	private Player owner;
+	private TextEntry userName;
+	private Button ready;
+	private Button weapon1, weapon2, weapon3;
+	private Boolean weaponSelected=false;
 	
-	private PresGameState state;
-	
-	public PreGameMenu(PresGameState state){
+	public PreGameMenu(TotemDefender game, Player owner) {
 		super(null);
-		this.state = state;
+		this.owner = owner;
+		
+		Vector2 buttonSize = new Vector2((TotemDefender.V_WIDTH/6),(TotemDefender.V_WIDTH/6)/4.65517f); //4.6.. is the apsect ratio of the button texture
+		float side = 10;
+		/*
+		userName = new TextEntry(this);
+		userName.setText("Player 1");
+		
+		if(owner.getID() == 2) {
+			side = TotemDefender.V_WIDTH - buttonSize.x - side;
+			userName.setText("Player 2");
+		}
+		userName.setSize(buttonSize);
+		userName.setPosition(new Vector2(side, TotemDefender.V_HEIGHT - buttonSize.y));
+		userName.setFont("hud_small.ttf");
+		userName.setTextOffset(buttonSize.x/2 - userName.getTextBounds().width/2, buttonSize.y/2 - userName.getTextBounds().height/2 + 5);
+		userName.create(game);
+		*/
+		weapon1 = new Button(this, "Player 1 Weapon 1", buttonSize, new Vector2(side, TotemDefender.V_HEIGHT - (buttonSize.y)*2), Color.RED){
+			@Override
+			public boolean onClick(){
+				
+				return true;
+			}
+		};
+		weapon1.setFont("hud_small.ttf");
+		weapon1.setTextOffset(buttonSize.x/2 - weapon1.getTextBounds().width/2, buttonSize.y/2 - weapon1.getTextBounds().height/2 + 5);
+		weapon1.create(game);
+		
+		weapon2 = new Button(this, "Player 1 Weapon 2", buttonSize, new Vector2(side, TotemDefender.V_HEIGHT - (buttonSize.y)*3), Color.BLUE){
+			@Override
+			public boolean onClick(){
+				
+				return true;
+			}
+		};
+		weapon2.setFont("hud_small.ttf");
+		weapon2.setTextOffset(buttonSize.x/2 - weapon2.getTextBounds().width/2, buttonSize.y/2 - weapon2.getTextBounds().height/2 + 5);
+		weapon2.create(game);
+		
+		weapon3 = new Button(this, "Player 1 Weapon 3", buttonSize, new Vector2(side, TotemDefender.V_HEIGHT - (buttonSize.y)*4), Color.YELLOW){
+			@Override
+			public boolean onClick(){
+				
+				return true;
+			}
+		};
+		weapon3.setFont("hud_small.ttf");
+		weapon3.setTextOffset(buttonSize.x/2 - weapon3.getTextBounds().width/2, buttonSize.y/2 - weapon3.getTextBounds().height/2 + 5);
+		weapon3.create(game);
+		
+
+		ready = new Button(this, "READY", buttonSize, new Vector2(side, TotemDefender.V_HEIGHT - (buttonSize.y)*5), Color.GREEN){
+			@Override
+			public boolean onClick(){
+				weaponSelected = true;
+				return true;
+			}
+		};
+		ready.setFont("hud_small.ttf");
+		ready.setTextOffset(buttonSize.x/2 - ready.getTextBounds().width/2, buttonSize.y/2 - ready.getTextBounds().height/2 + 5);
+		ready.create(game);
+		
+		addComponent(userName);
+		addComponent(weapon1);
+		addComponent(weapon2);
+		addComponent(weapon3);
+		addComponent(ready);
+		
+		connectComponents(weapon1, weapon2);
+		connectComponents(weapon2, weapon3);
+		connectComponents(weapon3, ready);
+		connectComponents(ready, weapon1, true);
+		
+		attachKeyboardListeners(owner);
 	}
 	
-	@Override
-	public void create(TotemDefender game){
-		
-		Vector2 buttonSize = new Vector2((TotemDefender.V_WIDTH/6),TotemDefender.V_HEIGHT/8);
-		float rightSide = TotemDefender.V_WIDTH - buttonSize.x;
-		float buttonArea = TotemDefender.V_WIDTH/2 - buttonSize.x/2; //width position of where the button are position
-		
-		player1 = new TextEntry(this);
-		player1.setSize(buttonSize);
-		player1.setPosition(new Vector2(0, TotemDefender.V_HEIGHT - (buttonSize.y)*2));
-		
-		p1_weapon1 = new Button(this, "Player 1 Weapon 1", buttonSize, new Vector2(0, TotemDefender.V_HEIGHT - (buttonSize.y)*2), Color.RED){
-			@Override
-			public boolean onClick(){
-				
-				return true;
-			}
-		};
-		
-		p1_weapon2 = new Button(this, "Player 1 Weapon 2", buttonSize, new Vector2(0, TotemDefender.V_HEIGHT - (buttonSize.y)*3), Color.BLUE){
-			@Override
-			public boolean onClick(){
-				
-				return true;
-			}
-		};
-		
-		p1_weapon3 = new Button(this, "Player 1 Weapon 3", buttonSize, new Vector2(0, TotemDefender.V_HEIGHT - (buttonSize.y)*4), Color.YELLOW){
-			@Override
-			public boolean onClick(){
-				
-				return true;
-			}
-		};
-		
-		player2 = new TextEntry(this);
-		player2.setSize(buttonSize);
-		player2.setPosition(new Vector2(0, TotemDefender.V_HEIGHT - (buttonSize.y)*2));
-		
-		p2_weapon1 = new Button(this, "Player 2 Weapon 2", buttonSize, new Vector2(rightSide, TotemDefender.V_HEIGHT - (buttonSize.y)*2), Color.RED){
-			@Override
-			public boolean onClick(){
-				
-				return true;
-			}
-		};
-		
-		p2_weapon2 = new Button(this, "Player 2 Weapon 3", buttonSize, new Vector2(rightSide, TotemDefender.V_HEIGHT - (buttonSize.y)*3), Color.BLUE){
-			@Override
-			public boolean onClick(){
-				
-				return true;
-			}
-		};
-		
-		p2_weapon3 = new Button(this, "Player 2 Weapon 3", buttonSize, new Vector2(rightSide, TotemDefender.V_HEIGHT - (buttonSize.y)*4), Color.YELLOW){
-			@Override
-			public boolean onClick(){
-				
-				return true;
-			}
-		};
-		
-		
-		ready = new Button(this, "READY", buttonSize, new Vector2(buttonArea, 30), Color.GREEN){
-			@Override
-			public boolean onClick(){
-				state.readyButtonPressed(true);
-				return true;
-			}
-		};
-		
-		this.addComponent(player1);
-		this.addComponent(p1_weapon1);
-		this.addComponent(p1_weapon2);
-		this.addComponent(p1_weapon3);
-		this.addComponent(player2);
-		this.addComponent(p2_weapon1);
-		this.addComponent(p2_weapon2);
-		this.addComponent(p2_weapon3);
-		this.addComponent(ready);
-		
-		attachKeyboardListeners(game.getPlayer2());
-		super.create(game);
+	public boolean hasSelected(){
+		return weaponSelected;
 	}
 }
--- core/src/com/totemdefender/states/PresGameState.java
+++ core/src/com/totemdefender/states/PresGameState.java
@@ -1,46 +1,71 @@
 package com.totemdefender.states;
 
+import com.badlogic.gdx.math.Vector2;
 import com.totemdefender.TotemDefender;
 import com.totemdefender.menu.MainMenu;
 import com.totemdefender.menu.PreGameMenu;
+import com.totemdefender.menu.TextEntry;
+import com.totemdefender.menu.buildmenu.BuildMenu;
 
 public class PresGameState implements State {
-	
-private PreGameMenu menu;
-protected boolean readyButtonPressed=false;
+
+private TextEntry p1Name, p2Name;
+private PreGameMenu p1PreMenu;
+private PreGameMenu p2PreMenu;
+Vector2 buttonSize = new Vector2((TotemDefender.V_WIDTH/6),(TotemDefender.V_WIDTH/6)/4.65517f); //4.6.. is the apsect ratio of the button texture
+
 
 	@Override
 	public boolean canEnter(TotemDefender game) {
-		// TODO Auto-generated method stub
 		return true;
 	}
 
 	@Override
 	public void onEnter(TotemDefender game) {
-		menu = new PreGameMenu(this);
-		menu.create(game);
+		p1PreMenu = new PreGameMenu(game, game.getPlayer1());
+		
+		p1Name = new TextEntry(this.p1PreMenu);
+		p1Name.setText("Player 1");
+		p1Name.setSize(buttonSize);
+		p1Name.setPosition(new Vector2(10, TotemDefender.V_HEIGHT - buttonSize.y));
+		p1Name.setFont("hud_small.ttf");
+		p1Name.setTextOffset(buttonSize.x/2 - p1Name.getTextBounds().width/2, buttonSize.y/2 - p1Name.getTextBounds().height/2 + 5);
+
+		p2PreMenu = new PreGameMenu(game, game.getPlayer2());
+		p2Name = new TextEntry(this.p2PreMenu);
+		p2Name.setText("Player 1");
+		p2Name.setSize(buttonSize);
+		p2Name.setPosition(new Vector2(10, TotemDefender.V_HEIGHT - buttonSize.y));
+		p2Name.setFont("hud_small.ttf");
+		p2Name.setTextOffset(buttonSize.x/2 - p2Name.getTextBounds().width/2, buttonSize.y/2 - p2Name.getTextBounds().height/2 + 5);
+
+		p1Name.create(game);
+		p2Name.create(game);
+		p1PreMenu.create(game);
+		p2PreMenu.create(game);
 	}
 
 	@Override
 	public void onExit(TotemDefender game) {
-		menu.destroy(game);
-		if(readyButtonPressed){
-			game.getStateManager().attachState(new BuildState());
-		}
-	}
-
-	@Override
-	public boolean canExit(TotemDefender game) {
-		return readyButtonPressed;
-	}
-
-	@Override
-	public void update(TotemDefender game) {
-		// TODO Auto-generated method stub
+		p1PreMenu.destroy(game);
+		p2PreMenu.destroy(game);
+		game.setDoneBuilding(true);
+		game.getStateManager().attachState(new BuildState());
 		
 	}
-	
-	public void readyButtonPressed(boolean t){
-		readyButtonPressed = t;
+	@Override
+	public boolean canExit(TotemDefender game) {
+		if(p1PreMenu == null || p2PreMenu == null) return false;
+		return p1PreMenu.hasSelected() && p2PreMenu.hasSelected();
+	}
+	@Override
+	public void update(TotemDefender game) {
+	}
+
+	public PreGameMenu getP1PreMenu() {
+		return p1PreMenu;
+	}
+	public PreGameMenu getP2PreMenu() {
+		return p2PreMenu;
 	}
 }
