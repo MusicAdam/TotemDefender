1213e6cddd25576ba769c46d01877c9ac8d43d0c
Viter Nguyen <retiv42@gmail.com>
2015-03-06 08:32:04 -0500

    minor changes in display

--- core/src/com/totemdefender/menu/PreGameMenu.java
+++ core/src/com/totemdefender/menu/PreGameMenu.java
@@ -30,14 +30,10 @@ public class PreGameMenu extends NavigableContainer {
 		Vector2 buttonSize = new Vector2((TotemDefender.V_WIDTH/6),(TotemDefender.V_HEIGHT/1.5f)/4.65517f); //4.6.. is the apsect ratio of the button texture
 		float padding = 10;
 		float side = padding;
-
-		userName = new TextEntry(this);
-		userName.setText("Player 1");
+		int player = owner.getID();
 		
 		if(owner.getID() == 2) {
 			side = TotemDefender.V_WIDTH - buttonSize.x - padding;
-			
-			userName.setText("Player 2");
 			instruct = new Button(this, "", 
 					new Vector2(((TotemDefender.V_WIDTH * (2.0f/3.0f)) - 20), buttonSize.y*5), 
 					new Vector2(buttonSize.x + padding, TotemDefender.V_HEIGHT - (buttonSize.y)*5 - padding), null);
@@ -48,13 +44,16 @@ public class PreGameMenu extends NavigableContainer {
 			System.out.print("size x: " + instruct.getSize().x + " size y: " + instruct.getSize().y + "\n"); 
 			System.out.print("pos x: " + instruct.getPosition().x + " pos y: " + instruct.getPosition().y + "\n"); 
 		}
+		
+		userName = new TextEntry(this);
+		userName.setText("Player " + player);
 		userName.setSize(buttonSize);
 		userName.setPosition(new Vector2(side, TotemDefender.V_HEIGHT - (buttonSize.y + padding)));
 		userName.setFont("hud_small.ttf");
 		userName.setTextOffset(buttonSize.x/2 - userName.getTextBounds().width/2, buttonSize.y/2 - userName.getTextBounds().height/2 + 5);
 		userName.create(game);
 
-		weapon1 = new Button(this, "Player 1 Weapon 1", buttonSize, new Vector2(side, TotemDefender.V_HEIGHT - (buttonSize.y)*2 - padding), Color.RED){
+		weapon1 = new Button(this, "Player " + player + " Weapon 1", buttonSize, new Vector2(side, TotemDefender.V_HEIGHT - (buttonSize.y)*2 - padding), Color.RED){
 			@Override
 			public boolean onClick(){
 				
@@ -65,7 +64,7 @@ public class PreGameMenu extends NavigableContainer {
 		weapon1.setTextOffset(buttonSize.x/2 - weapon1.getTextBounds().width/2, buttonSize.y/2 - weapon1.getTextBounds().height/2 + 5);
 		weapon1.create(game);
 	
-		weapon2 = new Button(this, "Player 1 Weapon 2", buttonSize, new Vector2(side, TotemDefender.V_HEIGHT - (buttonSize.y)*3 - padding), Color.BLUE){
+		weapon2 = new Button(this, "Player " + player + " Weapon 2", buttonSize, new Vector2(side, TotemDefender.V_HEIGHT - (buttonSize.y)*3 - padding), Color.BLUE){
 			@Override
 			public boolean onClick(){
 				
@@ -76,7 +75,7 @@ public class PreGameMenu extends NavigableContainer {
 		weapon2.setTextOffset(buttonSize.x/2 - weapon2.getTextBounds().width/2, buttonSize.y/2 - weapon2.getTextBounds().height/2 + 5);
 		weapon2.create(game);
 		
-		weapon3 = new Button(this, "Player 1 Weapon 3", buttonSize, new Vector2(side, TotemDefender.V_HEIGHT - (buttonSize.y)*4 - padding), Color.ORANGE){
+		weapon3 = new Button(this, "Player " + player + " Weapon 3", buttonSize, new Vector2(side, TotemDefender.V_HEIGHT - (buttonSize.y)*4 - padding), Color.ORANGE){
 			@Override
 			public boolean onClick(){
 				
@@ -108,5 +107,13 @@ public class PreGameMenu extends NavigableContainer {
 	
 	public boolean hasSelected(){
 		return weaponSelected;
+	}
+
+	public TextEntry getUserName() {
+		return userName;
+	}
+
+	public void setUserName(TextEntry userName) {
+		this.userName = userName;
 	}
 }
