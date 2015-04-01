6c6c5d30e1ee953434be564789bdd580f33e069b
Adam Zielinski <zielinskia@wit.edu>
2015-04-01 14:11:08 -0400

    Fixed cannon sprite

--- core/src/com/totemdefender/entities/CatapultWeapon.java
+++ core/src/com/totemdefender/entities/CatapultWeapon.java
@@ -6,13 +6,10 @@ import com.totemdefender.TotemDefender;
 public class CatapultWeapon extends WeaponEntity {
 
 	public CatapultWeapon(Player owner) {
-		
 		super(owner);
-		// TODO Auto-generated constructor stub
 	}
 	
 	public void spawn(TotemDefender game){
-		//TODO: Create this weapon
 	}
 
 }
