a8629e43dba94ecd9e30781e5abd6aa041436a58
Adam Zielinski <zielinskia@wit.edu>
2015-04-01 12:27:53 -0400

    Fixed weapons

--- core/src/com/totemdefender/entities/BalistaWeapon.java
+++ core/src/com/totemdefender/entities/BalistaWeapon.java
@@ -10,20 +10,8 @@ public class BalistaWeapon extends WeaponEntity {
 		// TODO Auto-generated constructor stub
 	}
 	
-public void spawn(TotemDefender game){
-		
-		int originX;
-		int originY=14;
-		
-		if(owner.getID()==1){
-			originX=98;
-		}
-		else
-			originX=30;
-		
-		super.spawn(game,"cannon.png",originX,originY,95,20);
-		System.out.println("Balista Weapon was Spawned");
-		
+	public void spawn(TotemDefender game){
+		//TODO: Create this weapon...
 	}
 
 }
--- core/src/com/totemdefender/entities/CatapultWeapon.java
+++ core/src/com/totemdefender/entities/CatapultWeapon.java
@@ -11,20 +11,8 @@ public class CatapultWeapon extends WeaponEntity {
 		// TODO Auto-generated constructor stub
 	}
 	
-public void spawn(TotemDefender game){
-		
-		int originX;
-		int originY=14;
-		
-		if(owner.getID()==1){
-			originX=38;
-		}
-		else
-			originX=30;
-		
-		super.spawn(game,"catapult.png",originX,originY,15,20);
-		System.out.println("Catapult Weapon was Spawned");
-		
+	public void spawn(TotemDefender game){
+		//TODO: Create this weapon
 	}
 
 }
